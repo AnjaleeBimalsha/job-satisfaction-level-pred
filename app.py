9b0c65636e3f46782b02d9a85ee24c2f2141490c
Samitha99 <73548027+Samitha99@users.noreply.github.com>
2022-11-02 02:26:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+# setup application
+app = Flask(__name__)
+
+def prediction(lst):
+    filename = 'model/predictor.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst])
+    return pred_value
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    # return "Hello World"
+    # create gender list
+    Male = 0
+    Female = 0
+
+    pred_value = -1
+    print(request.method)
+    if request.method == 'POST':
+    
+        Salary = int(request.form['Salary'])
+        PerformanceScore = int(request.form['PerformanceScore'])
+        EngagementSurvey = float(request.form['EngagementSurvey'])
+        SpecialProjectsCount= int(request.form['SpecialProjectsCount'])
+        DaysLateLast30= int(request.form['DaysLateLast30'])
+        Absences = int(request.form['Absences'])
+        Position= request.form['Position']
+        
+        DateOfHire= request.form['DateOfHire']
+        #get separatly year month and day
+        DateOfHire = DateOfHire.split('-')
+        DateOfHireYear = int(DateOfHire[0])
+        DateOfHireMonth = int(DateOfHire[1])
+        DateOfHireDay = int(DateOfHire[2])
+        
+        DateOfBirth= request.form['DateOfBirth']
+        #get separatly year month and day
+        DateOfBirth = DateOfBirth.split('-')
+        DateOfBirthYear = int(DateOfBirth[0])
+        DateOfBirthMonth = int(DateOfBirth[1])
+        DateOfBirthDay = int(DateOfBirth[2])
+        
+        getGender = request.form['Gender']
+        #Gender check
+        if getGender == 'Female':
+            Female = 1
+        else:
+            Male = 1
+        
+        MaritalStatus= request.form['MaritalStatus']
+        Department= request.form['Department']
+        ManagerName= request.form['ManagerName']
+        RecruitmentSource= request.form['RecruitmentSource']
+
+        position_list=['Architect','Director','Engineer','Manager','Other','TechnicianI','TechnicianII']
+        Marital_Status_list=['Divorced','Married','Separated','Single','Widowed']
+        Department_list=['AdminOffices','ExecutiveOffice','IT/IS','Production','Sales','SoftwareEngineering']
+        ManagerName_list=['AlexSweetwater',
+       'AmyDunn', 'BoardofDirectors', 'BrandonR.LeBlanc',
+       'BrannonMiller', 'BrianChampaigne', 'DavidStanley',
+       'DebraHoulihan', 'ElijiahGray', 'EricDougall',
+       'JanetKing', 'JenniferZamora', 'JohnSmith',
+       'KelleySpirea', 'KetsiaLiebig', 'KissySullivan',
+       'LynnDaneault', 'MichaelAlbert', 'PeterMonroe',
+       'SimonRoup', 'WebsterButler']
+        RecruitmentSource_list=['CareerBuilder',
+       'DiversityJobFair', 'EmployeeReferral', 'GoogleSearch',
+       'Indeed', 'LinkedIn', 'On-lineWebApplication', 'Other',
+       'Website']
+       
+        def traverse_list(lst, value):
+            for item in lst:
+                if item == value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+        
+        feature_list = []
+        feature_list.append(Salary)
+        feature_list.append(PerformanceScore)
+        feature_list.append(EngagementSurvey)
+        feature_list.append(SpecialProjectsCount)
+        feature_list.append(DaysLateLast30)
+        feature_list.append(Absences)
+        feature_list.append(DateOfBirthYear)
+        feature_list.append(DateOfBirthMonth)
+        feature_list.append(DateOfBirthDay)
+        feature_list.append(DateOfHireYear)
+        feature_list.append(DateOfHireMonth)
+        feature_list.append(DateOfHireDay)
+        traverse_list(position_list,Position)
+        feature_list.append(Female)
+        feature_list.append(Male)
+        traverse_list(Marital_Status_list, MaritalStatus )
+        traverse_list(Department_list, Department)
+        traverse_list(ManagerName_list, ManagerName)
+        traverse_list(RecruitmentSource_list, RecruitmentSource)
+
+        # for item in company_list:
+        #     if item == company:
+        #         feature_list.append(1)
+        #     else:
+        #         feature_list.append(0)
+
+        print(feature_list)
+        pred_value = prediction(feature_list)
+        print(pred_value)
+    # return render_template('index.html')
+
+    return render_template('index.html', pred_value=pred_value)
+
+
+# @app.route('/', methods=['GET'])
+# def index_get():
+#     return render_template('home.html', pred_value=-1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     return "Hello World"
---
+++ model/chart/Extensive_EDA.md
@@ -0,0 +1,129 @@
+## Bivariate analysis of Salary feature with target
+
+![](Salary_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of PerformanceScore feature with target
+
+![](PerformanceScore_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of EngagementSurvey feature with target
+
+![](EngagementSurvey_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of SpecialProjectsCount feature with target
+
+![](SpecialProjectsCount_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DaysLateLast30 feature with target
+
+![](DaysLateLast30_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of Absences feature with target
+
+![](Absences_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOB_year feature with target
+
+![](DOB_year_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOB_month feature with target
+
+![](DOB_month_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOB_day feature with target
+
+![](DOB_day_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOH_year feature with target
+
+![](DOH_year_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOH_month feature with target
+
+![](DOH_month_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of DOH_day feature with target
+
+![](DOH_day_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Architect feature with target
+
+![](P_Architect_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Director feature with target
+
+![](P_Director_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Engineer feature with target
+
+![](P_Engineer_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Manager feature with target
+
+![](P_Manager_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Other feature with target
+
+![](P_Other_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Technician I feature with target
+
+![](P_Technician_I_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of P_Technician II feature with target
+
+![](P_Technician_II_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of S_F feature with target
+
+![](S_F_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of S_M  feature with target
+
+![](S_M__target.png)
+
+------------------------------------------------------
+## Bivariate analysis of MD_Divorced feature with target
+
+![](MD_Divorced_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of MD_Married feature with target
+
+![](MD_Married_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of MD_Separated feature with target
+
+![](MD_Separated_target.png)
+
+------------------------------------------------------
+## Bivariate analysis of MD_Single feature with target
+
+![](MD_Single_target.png)
+
+------------------------------------------------------
+## Heatmap
+![](heatmap.png)
+
+------------------------------------------------------
